ba5444391e85e7d1e877999cbadd39bd9a3bbecd
Maria Marta <liacode19@gmail.com>
2026-07-16 11:19:45 -0300

    chore(setup): inicializa projeto Spring Boot com JPA e PostgreSQL

---
+++ src/main/java/br/com/reireal/reireal_backend/ReirealBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.reireal.reireal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReirealBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReirealBackendApplication.class, args);
+	}
+
+}
