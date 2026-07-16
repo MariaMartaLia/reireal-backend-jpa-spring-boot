87d30cfe259f4a982bf9a817ebc3b20561b91d77
Maria Marta <liacode19@gmail.com>
2026-07-16 17:39:07 -0300

    feat(repository): cria CategoriaRepository

--- src/main/java/br/com/reireal/domain/entity/Categoria.java
+++ src/main/java/br/com/reireal/domain/entity/Categoria.java
@@ -3,44 +3,70 @@ package br.com.reireal.domain.entity;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria") 
 public class Categoria {
+@Id
+@GeneratedValue
+@UuidGenerator
 private UUID id;
+
+@Column(nullable = false, unique = true, length = 100)
 private String nome;
+
+@Column(nullable = false)
 private boolean ativo;
 
-public Categoria(String nome){
+public Categoria() {
+}
+
+public Categoria(String nome) {
     validarNome(nome);
-    this.id = UUID.randomUUID();
     this.nome = nome;
     this.ativo = true;
 }
-public UUID getId(){
+public UUID getId() {
     return id;
 }
-public String getNome(){
+public String getNome() {
     return nome;
 }
-public boolean isAtivo(){
+public boolean isAtivo() {
     return ativo;
 }
-public void alterarNome(String nome){
+public void alterarNome(String nome) {
     validarNome(nome);
     this.nome = nome;
 }
-public void desativarCategoria(){
-    if(!ativo){
-        throw new IllegalStateException("Categoria já esta desativada.");
+public void desativarCategoria() {
+    if (!ativo) {
+        throw new IllegalStateException(
+            "Categoria já está desativada."
+        );
     }
     ativo = false;
 }
-public void ativarCategoria(){
-    if(ativo){
-        throw new IllegalStateException("Categoria já está ativa.");
+public void ativarCategoria() {
+    if (ativo) {
+        throw new IllegalStateException(
+            "Categoria já está ativa."
+        );
     }
     ativo = true;
 }
-private void validarNome(String nome){
-    if(nome == null || nome.isBlank()){
-        throw new IllegalArgumentException("Nome não pode ser nulo ou vazio.");
-    }}
+private void validarNome(String nome) {
+    if (nome == null || nome.isBlank()) {
+        throw new IllegalArgumentException(
+            "O nome da categoria é obrigatório."
+        );
+    }
+}
 }
---
+++ src/main/java/br/com/reireal/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package br.com.reireal.repository;
+
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.reireal.domain.entity.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria,  UUIDS> {
+
+}
+
+
